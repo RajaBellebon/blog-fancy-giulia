3f55bbdbd4b833eac72b4605f8e5bc9f702d0538
Raja Bellebon <rbellebon@atlassian.com>
2017-11-19 18:42:32 +1100

    add the blog image

--- react-serverless-blog/src/App.js
+++ react-serverless-blog/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import blog from './images/blog.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>My blog</h2>
+          <h2>Giulia &amp; Bolli Fancy Blog</h2>
+          <img alt="blog" src={blog}/>
         </div>
         <div>
           {this.props.children}
